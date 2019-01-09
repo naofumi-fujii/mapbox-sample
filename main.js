28432677d0cacce94b6999ebbc633246e326b3b1
naofumi-fujii <naofumi-fujii@users.noreply.github.com>
2019-01-09 11:26:28 +0900

    Revert "fix point design"

--- main.js
+++ main.js
@@ -67,8 +67,7 @@ map.on("load", function() {
 		filter: ["!", ["has", "point_count"]],
 		paint: {
 			"circle-color": "#11b4da",
-			"circle-opacity": 0.7,
-			"circle-radius": 60,
+			"circle-radius": 4,
 			"circle-stroke-width": 1,
 			"circle-stroke-color": "#fff"
 		}
